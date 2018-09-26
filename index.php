aa7b5d540db91d6f6574a13a57ee4109a16592ea
ShaxSharipov91 <ShaxSharipov@gmail.com>
2018-09-25 21:49:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
  </head>
  <body>
  <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+   <?php echo '<p>EDITED</p>'; ?> 
  </body>
 </html>
